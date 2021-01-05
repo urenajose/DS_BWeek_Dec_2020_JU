8ee6422459808740b4cd72ac71376788126ca1bd
Jose Urena <urena.jose@gmail.om>
2021-01-05 18:47:53 -0500

    testing hellow route

--- app.py
+++ app.py
@@ -21,28 +21,28 @@ filename = './csv/spotify_track_neighbors.urenaj'
 model_knn = pickle.load(open(filename, 'rb'))
 # pickle spotify model NearestNeighbors
 
-@app.route("/", methods=["GET","POST"])
-def song_recomender():
-    if request.method == "POST":
-        DB.create_all()
-        print(request.form)
-        name_song = request.form.get("ssong")
-        id_track = process.extractOne(name_song, spotify['name'])[2]
-        print('Song selected', spotify['name'][id_track], 'id:', id_track)
-        print('searching for recomendation....')
-        # extracting the index numbers (rows) from the model
-        distances, ids = model_knn.kneighbors(spotify_matrix[[id_track]], 
-                                              n_neighbors=20)
-        # extracting the list of the matrix, and creating a list of list.
-        spotify_ids = list(ids[0])
-        # creating a df with index return and desire columns
-        recomendation = spotify[['artists', 'name', 'id']].loc[spotify_ids]
-        engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], echo=False)
-        recomendation
-        recomendation.to_sql('tracks', con=engine, if_exists="replace", index=False)
-        return redirect("/TracksReco")
-    else:
-        return render_template('form.html')
+@app.route("/", methods=["GET", "POST"])
+# def song_recomender():
+#     if request.method == "POST":
+#         DB.create_all()
+#         print(request.form)
+#         name_song = request.form.get("ssong")
+#         id_track = process.extractOne(name_song, spotify['name'])[2]
+#         print('Song selected', spotify['name'][id_track], 'id:', id_track)
+#         print('searching for recomendation....')
+#         # extracting the index numbers (rows) from the model
+#         distances, ids = model_knn.kneighbors(spotify_matrix[[id_track]], 
+#                                               n_neighbors=20)
+#         # extracting the list of the matrix, and creating a list of list.
+#         spotify_ids = list(ids[0])
+#         # creating a df with index return and desire columns
+#         recomendation = spotify[['artists', 'name', 'id']].loc[spotify_ids]
+#         engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], echo=False)
+#         recomendation
+#         recomendation.to_sql('tracks', con=engine, if_exists="replace", index=False)
+#         return redirect("/TracksReco")
+    # else:
+        return "hellow world"
 
 @app.route("/TracksReco")
 def TracksReco():
